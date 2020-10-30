2099f65681c341fa4bb162382cc6c2519a4ea7da
Lucas Rodrigues <33637247+lucasrdrgs@users.noreply.github.com>
2020-10-30 16:18:22 -0300

    Update app.py

--- example/app.py
+++ example/app.py
@@ -3,16 +3,15 @@ from flask import Flask, request, session
 
 app = Flask(__name__)
 
-# First param is the parent folder of your project.
-# In this scenario, it's abs path of __file__ (app.py)
-# so that it can find the templates.
+# First param is the parent directory of your project,
+# so that it can find your templates and components.
 boa = Boa(__file__, template_dir = 'html')
 
 @app.route('/', methods = ['GET'])
 def index():
 	# It's a good practice to not use Boa at all. However, if you're
-	# willing to risk yourself, it's nice to specify your request and your session
-	# so that you can work with GETs and POSTs. Not needed, though.
+	# willing to risk it, it's nice to specify your request and your session
+	# so that you can work with GETs and POSTs and whatever. Not needed, though.
 	return boa.render('index.html', request, session, my_ctx_var = 123)
 	# This should also work just fine:
 	# return boa.render('index.html', my_ctx_var = 123)
